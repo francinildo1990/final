ee20df3987cca10bd44b7790772c66978c10caa0
IFPB <ifpb@MDK00LB10-JP>
2019-12-19 10:43:14 -0300

    add

--- Servidor/simulador.py
+++ Servidor/simulador.py
@@ -6,6 +6,6 @@ while(True):
    valor = int(input("Informe um valor: "))
    valor =+2
    
-   r = requests.post("http://iot-2020.herokuapp.com/upload", json={"LDR":valor})
+   r = requests.post("http://sensorcorrente.herokuapp.com/upload", json={"LDR":valor})
    print(r.status_code)
 
